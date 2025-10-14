bcb46692e8ef11f9d4cfc57d41f5158e4aaea062
Leandro Solovjovas dos Santos <solovjovas.leandro@gmail.com>
2025-10-14 12:50:49 +0000

    Adicionando exercício AreaCircunferencia no repositório, praticando e utilizando variáveis e constantes

---
+++ fundamentos/AreaCircunferencia.java
@@ -0,0 +1,16 @@
+public class AreaCircunferencia{
+
+    public static void main(String[] args){
+        // Variável 
+        double raio = 3.4;
+
+        // Constante
+        final double pi = 3.14159;
+
+        // Expressão
+        double area = pi * raio * raio;
+
+        System.out.println("Área = " + area + "m2");
+    }
+
+}
